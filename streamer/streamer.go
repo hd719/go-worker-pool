7d456a73be10f0c91fe85e3e4c4fb228797cb073
Hamel Desai <hameldesai93@gmail.com>
2024-05-06 13:05:31 -0400

    section-11-video-98

--- streamer/streamer.go
+++ streamer/streamer.go
@@ -7,11 +7,23 @@ type ProcessingMessage struct {
 	OutputFile string
 }
 
+// This will hold the unit of work that we want our worker pool to perform
 type VideoProcessingJob struct {
+	Video Video
 }
 
 // This will return the format of the data we need (ex. convert mp4 into a web mp4)
 type Processor struct {
+}
+
+type Video struct {
+	ID         int
+	InputFile  string // the video we want to encode
+	OutputDir  string // where we want the encoded video to show up
+	Type       string
+	NotifyChan chan ProcessingMessage // Where are we going to send the processed video to
+	// Options *VideoOptions
+	Encoder Processor
 }
 
 func New(jobQueue chan VideoProcessingJob, maxWorkers int) *VideoDispatcher {
