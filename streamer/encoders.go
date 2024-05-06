33d919879c165edd4404707d7e0a39ae1f08e1f3
Hamel Desai <hameldesai93@gmail.com>
2024-05-06 13:05:32 -0400

    section-11-video-109

--- streamer/encoders.go
+++ streamer/encoders.go
@@ -2,6 +2,8 @@ package streamer
 
 import (
 	"fmt"
+	"os/exec"
+	"strconv"
 
 	"github.com/xfrr/goffmpeg/transcoder"
 )
@@ -45,5 +47,61 @@ func (ve *VideoEncoder) EncodeToMP4(v *Video, baseFileName string) error {
 }
 
 func (vd *VideoEncoder) EncodeToHLS(v *Video, baseFileName string) error {
+	// Create a channel to get results
+	result := make(chan error)
+
+	// Spawn a goroutine to do the encode
+	go func(result chan error) {
+		ffmpegCmd := exec.Command(
+			"ffmpeg",
+			"-i", v.InputFile,
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-map", "0:v:0",
+			"-map", "0:a:0",
+			"-c:v", "libx264",
+			"-crf", "22",
+			"-c:a", "aac",
+			"-ar", "4800",
+			"-filter:v:0", "scale=-2:1080",
+			"-maxrate:v:0", v.Options.MaxRate1080p,
+			"-b:a:0", "128k",
+			"-filter:v:1", "scale=-2:720",
+			"-maxrate:v:1", v.Options.MaxRate720p,
+			"-b:a:1", "128k",
+			"-filter:v:2", "scale=-2:480",
+			"-maxrate:v:2", v.Options.MaxRate480p,
+			"-b:a:2", "64k",
+			"-var_stream_map", "v:0,a:0,name:1080p v:1,a:1,name=720p v:2,a:2,name:480p",
+			"-preset", "slow",
+			"-hls_list_size", "0",
+			"-threads", "0",
+			"-f", "hls",
+			"-hls_playlist_type", "event",
+			"-hls_time", strconv.Itoa(v.Options.SegmentDuration),
+			"-hls_flags", "independent_segments",
+			"-hls_segment_type", "mpegts",
+			"-hls_playlist_type", "vod",
+			"-master_pl_name", fmt.Sprintf("%s.m3u8", baseFileName),
+			"-profile:v", "baseline",
+			"-level", "3.0",
+			"-progres", "-",
+			"-nostats",
+			fmt.Sprintf("%s/%s-%%v.m3u8", v.OutputDir, baseFileName),
+		)
+
+		_, err := ffmpegCmd.CombinedOutput()
+		result <- err
+	}(result)
+
+	// Listen to the result channel
+	err := <-result
+	if err != nil {
+		return err
+	}
+
+	// Return the results (success or not)
 	return nil
 }
